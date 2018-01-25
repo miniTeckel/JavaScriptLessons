e584f06c175500fe87625bd76438edcd8f97736a
Lana Ka <lana@home>
2018-01-25 23:47:59 +0200

    Задача 2. Чтение всех файлов в папке на промисах

---
+++ lesson1/read-all.js
@@ -0,0 +1,38 @@
+const fs = require('fs');
+const file = require('./file-promise');
+const path = require('path');
+
+function readfiles(base_path, files){
+    var file_array = new Array(); 
+    for (let i = 0; i < files.length; i++) {
+        file_array.push(
+            file
+                .read(path.join(base_path, files[i]))
+                .then(text => {
+                    var obj = {};
+                    obj.name = files[i];
+                    obj.content = text;
+                    return obj;
+                }));
+    }
+    return file_array;
+}
+
+function read_dir (path){
+    return new Promise((done, fail) => {
+        fs.readdir(path, (err, files) => {
+            if(err){
+                fail(err);
+            }
+            else {
+                Promise
+                    .all(readfiles(path, files))
+                    .then(result => done(result))
+                    .catch(err => fail(err));
+            };
+        })
+    });
+}        
+
+
+module.exports = read_dir;
