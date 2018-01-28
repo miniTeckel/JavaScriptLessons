d2c04dda17603f96552d021bd79e225988782bdf
Lana Ka <lana@home>
2018-01-28 22:01:58 +0200

    Задача 3. Информация о файле без промисов

---
+++ lesson1/path-info.js
@@ -0,0 +1,50 @@
+const fs = require('fs');
+
+function file_info_dir(path, callback){
+    fs.readdir(path, (err, files) => {
+        if(err){
+            callback(err, null);
+        } else {
+            var info ={
+                path: path,
+                type: 'directory',
+                childs: files
+            };
+            callback(null, info);
+        }
+    });
+}
+
+function file_info_file(path, callback){
+    fs.readFile(path, 'utf8', (err, content) => {
+        if(err){
+            callback(err, null);
+        } else {
+            var info ={
+                path: path,
+                type: 'file',
+                content: content
+            };
+            callback(null, info);
+        }
+    });
+}
+
+function files_info(path, callback){
+    
+    fs.stat(path, (error, stats) =>{
+        if (error) {
+            callback(error, null);
+        } else {
+            if (stats.isDirectory()){
+                file_info_dir(path, callback);
+            }else if (stats.isFile()){
+                file_info_file(path, callback);
+            } else {
+                callback(null, {type: undefined})
+            }
+        }   
+    });
+}
+
+module.exports = files_info;
