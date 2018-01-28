a0870dcf6537a97f0a3cbcbcc77015af7cfe226e
Lana Ka <lana@home>
2018-01-28 22:13:24 +0200

    Очистка

--- lesson1/file-promise.js
+++ lesson1/file-promise.js
@@ -30,14 +30,4 @@ function write (filename, text){
     read,
     write
  };
-
- /*     fs.access('./upper-data.txt', fs.constants.W_OK, err =>{
-                   if (err){
-                        return console.log('нет прав на запись %s',);
-                    }
-               else {
-                console.log('Создан файл ./upper-data.txt');  
-                return fail;    
-            }
-            //return file;
-            });*/ 
+ 
